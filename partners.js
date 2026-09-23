0d25653e651e1e690bc1205bc6fcfdc1d1fc4996
shuling02 <sllin23999@gmail.com>
2026-09-23 12:25:20 +0800

    Added partners logos

--- partners.js
+++ partners.js
@@ -1,11 +1,30 @@
 var imageList = "";
 
+var images = [
+    "partner-bustour.png",
+    "partner-cabinrental.png",
+    "partner-campingadv.png",
+    "partner-collegetours.png",
+    "partner-rentalbike.png",
+    "partner-tourgroup.png"
+];
+
+var altText = [
+    "Partner Bus Tours",
+    "Partner Cabin Rental",
+    "Partner Camping Adventure",
+    "Partner College Tours",
+    "Partner Bike Rentals",
+    "Partner Tour Group"
+];
+
 for (var i = 0; i < 6; i++){
     imageList += `
         <li class="partner">
-            <img src="images/logo.png" alt="Partner Bus Tours">
+            <img src="images/partners/${images[i]}" alt="${altText[i]}">
         </li>
-    `
+    `;
 }
 
 document.getElementById("partners").innerHTML = imageList;
+
